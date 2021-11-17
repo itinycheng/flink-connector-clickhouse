3d34c4061ebd941efd3dc76d23a752d3db1f1e32
chenglong.wang <tiny.wcl@gmail.com>
2021-11-17 15:37:49 +0800

    [bug fix] Add TIMESTAMP_LTZ type support

--- src/test/java/com/tiny/AppTest.java
+++ src/test/java/com/tiny/AppTest.java
@@ -1,14 +1,29 @@
 package com.tiny;
 
-import org.junit.Test;
+import org.apache.flink.table.data.TimestampData;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import ru.yandex.clickhouse.util.ClickHouseValueFormatter;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.TimeZone;
 
 /** Unit test for simple App. */
 public class AppTest {
+
     /** Rigorous Test :-). */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void timestampLtzTest() {
+        Instant now = Instant.now();
+        TimestampData timestampData = TimestampData.fromInstant(now);
+        Timestamp timestamp = timestampData.toTimestamp();
+        Instant instant = timestampData.toInstant();
+        String s1 = ClickHouseValueFormatter.formatTimestamp(timestamp, TimeZone.getDefault());
+        String s2 =
+                ClickHouseValueFormatter.formatTimestamp(
+                        Timestamp.from(instant), TimeZone.getDefault());
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
