7e253ab4de1779e30757bf406429c8d72dcadbd6
chenglong.wang <tiny.wcl@gmail.com>
2022-07-29 18:25:44 +0800

    Update version to 1.15.1-SNAPSHOT

--- src/main/java/org/apache/flink/connector/clickhouse/ClickHouseDynamicTableFactory.java
+++ src/main/java/org/apache/flink/connector/clickhouse/ClickHouseDynamicTableFactory.java
@@ -4,14 +4,12 @@ import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ReadableConfig;
 import org.apache.flink.connector.clickhouse.internal.options.ClickHouseDmlOptions;
 import org.apache.flink.connector.clickhouse.internal.options.ClickHouseReadOptions;
-import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.connector.sink.DynamicTableSink;
 import org.apache.flink.table.connector.source.DynamicTableSource;
 import org.apache.flink.table.factories.DynamicTableSinkFactory;
 import org.apache.flink.table.factories.DynamicTableSourceFactory;
 import org.apache.flink.table.factories.FactoryUtil;
 import org.apache.flink.table.factories.FactoryUtil.TableFactoryHelper;
-import org.apache.flink.table.utils.TableSchemaUtils;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -71,13 +69,11 @@ public class ClickHouseDynamicTableFactory
 
         Properties clickHouseProperties =
                 getClickHouseProperties(context.getCatalogTable().getOptions());
-        TableSchema physicalSchema =
-                TableSchemaUtils.getPhysicalSchema(context.getCatalogTable().getSchema());
         return new ClickHouseDynamicTableSource(
                 getReadOptions(config),
                 clickHouseProperties,
                 context.getCatalogTable(),
-                physicalSchema);
+                context.getPhysicalRowDataType());
     }
 
     @Override
