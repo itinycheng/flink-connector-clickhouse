c1a85c284bfd3e43cfbae92a2a50229958b390de
chenglong.wang <tiny.wcl@gmail.com>
2023-02-14 10:32:59 +0800

    [bug fix] Fixed errors in sharding and flushing data

--- src/main/java/org/apache/flink/connector/clickhouse/internal/partitioner/ShufflePartitioner.java
+++ src/main/java/org/apache/flink/connector/clickhouse/internal/partitioner/ShufflePartitioner.java
@@ -1,8 +1,10 @@
 package org.apache.flink.connector.clickhouse.internal.partitioner;
 
 import org.apache.flink.connector.clickhouse.internal.schema.ClusterSpec;
+import org.apache.flink.connector.clickhouse.internal.schema.ShardSpec;
 import org.apache.flink.table.data.RowData;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /** Shuffle data by random numbers. */
@@ -12,7 +14,8 @@ public class ShufflePartitioner extends ClickHousePartitioner {
 
     @Override
     public int select(RowData record, ClusterSpec clusterSpec) {
-        int numShards = clusterSpec.getShards().size();
-        return ThreadLocalRandom.current().nextInt(numShards);
+        List<ShardSpec> shards = clusterSpec.getShards();
+        int index = ThreadLocalRandom.current().nextInt(shards.size());
+        return shards.get(index).getNum();
     }
 }
