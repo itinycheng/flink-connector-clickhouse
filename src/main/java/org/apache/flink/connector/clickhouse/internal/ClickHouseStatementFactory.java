8b3666affd66b9aa5c587d4079968cad41e9c264
chenglong.wang <tiny.wcl@gmail.com>
2023-05-17 15:48:08 +0800

    Optimize the code of lookup function

--- src/main/java/org/apache/flink/connector/clickhouse/internal/ClickHouseStatementFactory.java
+++ src/main/java/org/apache/flink/connector/clickhouse/internal/ClickHouseStatementFactory.java
@@ -1,11 +1,15 @@
 package org.apache.flink.connector.clickhouse.internal;
 
+import org.apache.flink.connector.clickhouse.util.ClickHouseUtil;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static java.util.stream.Collectors.joining;
+import static org.apache.flink.connector.clickhouse.util.ClickHouseUtil.quoteIdentifier;
 
 /** Create an insert/update/delete ClickHouse statement. */
 public class ClickHouseStatementFactory {
@@ -15,19 +19,32 @@ public class ClickHouseStatementFactory {
     private ClickHouseStatementFactory() {}
 
     public static String getSelectStatement(
-            String tableName, String databaseName, String[] fieldNames) {
-        String columns =
-                Arrays.stream(fieldNames)
-                        .map(ClickHouseStatementFactory::quoteIdentifier)
+            String tableName, String databaseName, String[] selectFields) {
+        String selectClause =
+                Arrays.stream(selectFields)
+                        .map(ClickHouseUtil::quoteIdentifier)
                         .collect(joining(", "));
         return String.join(
-                EMPTY, "SELECT ", columns, " FROM ", fromTableClause(tableName, databaseName));
+                EMPTY, "SELECT ", selectClause, " FROM ", fromTableClause(tableName, databaseName));
+    }
+
+    public static String getSelectWhereStatement(
+            String tableName,
+            String databaseName,
+            String[] selectFields,
+            String[] conditionFields) {
+        String selectStatement = getSelectStatement(tableName, databaseName, selectFields);
+        String whereClause =
+                Arrays.stream(conditionFields)
+                        .map(f -> format("%s = ?", quoteIdentifier(f)))
+                        .collect(Collectors.joining(" AND "));
+        return selectStatement + (conditionFields.length > 0 ? " WHERE " + whereClause : "");
     }
 
     public static String getInsertIntoStatement(String tableName, String[] fieldNames) {
         String columns =
                 Arrays.stream(fieldNames)
-                        .map(ClickHouseStatementFactory::quoteIdentifier)
+                        .map(ClickHouseUtil::quoteIdentifier)
                         .collect(joining(", "));
         String placeholders = Arrays.stream(fieldNames).map((f) -> "?").collect(joining(", "));
         return String.join(
@@ -100,9 +117,5 @@ public class ClickHouseStatementFactory {
         }
 
         return format("%s.%s", quoteIdentifier(databaseName), quoteIdentifier(tableName));
-    }
-
-    private static String quoteIdentifier(String identifier) {
-        return String.join(EMPTY, "`", identifier, "`");
     }
 }
