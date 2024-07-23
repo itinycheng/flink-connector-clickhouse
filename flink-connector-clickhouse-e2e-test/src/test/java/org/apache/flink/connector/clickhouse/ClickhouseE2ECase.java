1ecbf9c9a92c222a0496c614be069075877e949e
陈海晴(jennyhq.chen) <jennyhq.chen@huolala.cn>
2024-07-23 20:53:18 +0800

    [ISSUE-138][Feature]: Support Unit5 Test For Clickhouse Connector

--- flink-connector-clickhouse-e2e-test/src/test/java/org/apache/flink/connector/clickhouse/ClickhouseE2ECase.java
+++ flink-connector-clickhouse-e2e-test/src/test/java/org/apache/flink/connector/clickhouse/ClickhouseE2ECase.java
@@ -67,28 +67,44 @@ public class ClickhouseE2ECase extends FlinkContainerTestEnviroment {
 
         proxy = new ClickhouseProxy(CLICKHOUSE_CONTAINER.getJdbcUrl(), CLICKHOUSE_CONTAINER.getUsername(),CLICKHOUSE_CONTAINER.getPassword());
         proxy.execute(
-                "create table test (id Int32, name String) engine = Memory");
-        proxy.execute("create table test_insert (id Int32, name String) engine = Memory");
-        proxy.execute("insert into test values (1, 'test');");
-        proxy.execute("insert into test values (2, 'kiki');");
+                "create table test (id Int32, name String, float32_column Float32, date_column Date,datetime_column DateTime, array_column Array(Int32)) engine = Memory");
+        proxy.execute("create table test_insert (id Int32, name String, float32_column Float32, date_column Date,datetime_column DateTime, array_column Array(Int32)) engine = Memory; ");
+        proxy.execute("INSERT INTO test (id, name, float32_column, date_column, datetime_column, array_column) VALUES (1, 'Name1', 1.1, '2022-01-01', '2022-01-01 00:00:00', [1, 2, 3]);");
+        proxy.execute("INSERT INTO test (id, name, float32_column, date_column, datetime_column, array_column) VALUES (2, 'Name2', 2.2, '2022-01-02', '2022-01-02 01:00:00', [4, 5, 6]);");
+        proxy.execute("INSERT INTO test (id, name, float32_column, date_column, datetime_column, array_column) VALUES (3, 'Name3', 3.3, '2022-01-03', '2022-01-03 02:00:00', [7, 8, 9]);");
+        proxy.execute("INSERT INTO test (id, name, float32_column, date_column, datetime_column, array_column) VALUES (4, 'Name4', 4.4, '2022-01-04', '2022-01-04 03:00:00', [10, 11, 12]);");
+        proxy.execute("INSERT INTO test (id, name, float32_column, date_column, datetime_column, array_column) VALUES (5, 'Name5', 5.5, '2022-01-05', '2022-01-05 04:00:00', [13, 14, 15]);");
+        // proxy.execute("insert into test values (2, 'kiki');");
         List<String> sqlLines = new ArrayList<>();
-        sqlLines.add("create table clickhouse_test (id int, name varchar) with ('connector' = 'clickhouse',\n"
+        sqlLines.add("create table clickhouse_test (id int, name varchar,float32_column FLOAT,\n"
+               // + "    date_column DATE,\n"
+                + "    datetime_column TIMESTAMP(3),\n"
+                + "    array_column ARRAY<INT>) with ('connector' = 'clickhouse',\n"
                 + "  'url' = '" + jdbcUrl + "',\n"
                 + "  'table-name' = 'test',\n"
                 + "  'username'='test_username',\n"
                 + "  'password'='test_password'\n"
                 + ");");
-        sqlLines.add("create table test (id int, name varchar) with ('connector' = 'clickhouse',\n"
+        sqlLines.add("create table test (id int, name varchar,float32_column FLOAT,\n"
+               // + "    date_column DATE,\n"
+                + "    datetime_column TIMESTAMP(3),\n"
+                + "    array_column ARRAY<INT>) with ('connector' = 'clickhouse',\n"
                 + "  'url' = '" + jdbcUrl + "',\n"
+                + "  'table-name' = 'test_insert',\n"
                 + "  'username'='test_username',\n"
-                + "  'password'='test_password',\n"
-                + "  'table-name' = 'test_insert');");
+                + "  'password'='test_password'\n"
+                + ");");
         sqlLines.add("insert into test select * from clickhouse_test;");
 
         submitSQLJob(sqlLines, SQL_CONNECTOR_CLICKHOUSE_JAR, CLICKHOUSE_JDBC_JAR, HTTPCORE_JAR, HTTPCLIENT_JAR, HTTPCLIENT_H2_JAR);
         waitUntilJobRunning(Duration.of(1, ChronoUnit.MINUTES));
-        List<String> expectedResult = Arrays.asList("1,test","2,kiki");
-        proxy.checkResultWithTimeout(expectedResult, "test_insert", Arrays.asList("id", "name"), 60000);
+        List<String> expectedResult = Arrays.asList(
+                "1,Name1,1.1,2022-01-01 00:00:00,[1,2,3]",
+                "2,Name2,2.2,2022-01-02 01:00:00,[4,5,6]",
+                "3,Name3,3.3,2022-01-03 02:00:00,[7,8,9]",
+                "4,Name4,4.4,2022-01-04 03:00:00,[10,11,12]",
+                "5,Name5,5.5,2022-01-05 04:00:00,[13,14,15]");
+        proxy.checkResultWithTimeout(expectedResult, "test_insert", Arrays.asList("id", "name","float32_column","datetime_column","array_column"), 60000);
     }
 
 
--- flink-connector-clickhouse-e2e-test/src/test/java/org/apache/flink/connector/clickhouse/ClickhouseProxy.java
+++ flink-connector-clickhouse-e2e-test/src/test/java/org/apache/flink/connector/clickhouse/ClickhouseProxy.java
@@ -4,17 +4,22 @@ import com.clickhouse.jdbc.ClickHouseConnection;
 import com.clickhouse.jdbc.ClickHouseDataSource;
 import com.clickhouse.jdbc.ClickHouseDriver;
 import com.clickhouse.jdbc.ClickHouseStatement;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.shaded.org.apache.commons.lang3.StringUtils;
 
+import java.sql.Array;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class ClickhouseProxy {
     private String jdbcUrl;
@@ -58,16 +63,31 @@ public class ClickhouseProxy {
         ResultSet resultSet = statement.executeQuery("select * from " + table);
         while (resultSet.next()) {
             List<String> result = new ArrayList<>();
-            for (String field : fields) {
-                Object value = resultSet.getObject(field);
-                if (value == null) {
-                    result.add("null");
-                } else {
-                    result.add(value.toString());
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    if (!fields.contains(columnName)) {
+                        continue;
+                    }
+                    String columnType = metaData.getColumnTypeName(i);
+                    switch (columnType) {
+                        case "Array":
+                            Array array = resultSet.getArray(i);
+                            result.add(array.toString());
+                            break;
+                        case "Timestamp":
+                            Timestamp timestamp = resultSet.getTimestamp(i);
+                            result.add(timestamp.toString());
+                            break;
+                        default:
+                            String value = resultSet.getString(i);
+                            result.add(value);
+                            break;
+                    }
                 }
-            }
-            results.add(StringUtils.join(result));
 
+            results.add(result.stream().collect(Collectors.joining(",")));
         }
         Collections.sort(results);
         Collections.sort(expectedResult);
